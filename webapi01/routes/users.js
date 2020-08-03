d829e68aac3a429dcaba83a7b4bfc9e205a69b4e
Luiz Fernando Duarte Junior <luizfduartejr@MacBook-Pro-de-Luiz.local>
2020-08-03 20:00:11 -0300

    get e post funcionando

---
+++ webapi01/routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(db.findUsers());
+});
+
+router.post('/', (request, response) => {
+  const user = db.insertUser(request.body);
+  response.statusCode(201).json(user);
+})
+
+module.exports = router;
