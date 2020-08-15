1cec6cd65549a566c476f2df723f665334a13add
Luiz Fernando Duarte Junior <luizfduartejr@MacBook-Pro-de-Luiz.local>
2020-08-15 18:35:52 -0300

    joi e middleware

--- webapi01/routes/users.js
+++ webapi01/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const db = require("../db");
+const db = require("../models/db");
+const userSchema = require("../models/userSchema");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,18 +13,31 @@ router.get('/:id', (request, response) => {
   response.json(db.findUser(id));
 })
 
-router.post('/', (request, response) => {
+function validationMiddleware(request, response, next){
+  if(["POST", "PUT"].indexOf(request.method) !== -1){
+    if(!request.body.nome || !request.body.idade)
+      return response.status(422).json({error: "nome and idade are required."});
+  }
+
+  const { error } = userSchema.validate(request.body);
+  if(error)
+    return response.status(422).json({error: error.details});
+  else
+    next();
+}
+
+router.post('/', validationMiddleware, (request, response) => {
   const user = db.insertUser(request.body);
   response.status(201).json(user);
 })
 
-router.put('/:id', (request, response) => {
+router.put('/:id', validationMiddleware, (request, response) => {
   const id = request.params.id;
   const user = db.updateUser(id, request.body, true);
   response.status(200).json(user);
 })
 
-router.patch('/:id', (request, response) => {
+router.patch('/:id', validationMiddleware, (request, response) => {
   const id = request.params.id;
   const user = db.updateUser(id, request.body, false);
   response.status(200).json(user);
