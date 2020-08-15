8abd9e73a093719fe22813ded0ef71d6649ff65a
Luiz Fernando Duarte Junior <luizfduartejr@MacBook-Pro-de-Luiz.local>
2020-08-15 14:48:57 -0300

    crud funcionando com fs

--- webapi01/routes/users.js
+++ webapi01/routes/users.js
@@ -7,9 +7,32 @@ router.get('/', function(req, res, next) {
   res.json(db.findUsers());
 });
 
+router.get('/:id', (request, response) => {
+  const id = request.params.id;
+  response.json(db.findUser(id));
+})
+
 router.post('/', (request, response) => {
   const user = db.insertUser(request.body);
-  response.statusCode(201).json(user);
+  response.status(201).json(user);
+})
+
+router.put('/:id', (request, response) => {
+  const id = request.params.id;
+  const user = db.updateUser(id, request.body, true);
+  response.status(200).json(user);
+})
+
+router.patch('/:id', (request, response) => {
+  const id = request.params.id;
+  const user = db.updateUser(id, request.body, false);
+  response.status(200).json(user);
+})
+
+router.delete('/:id', (request, response) => {
+  const id = request.params.id;
+  const user = db.deleteUser(id);
+  response.status(200).json(user);
 })
 
 module.exports = router;
